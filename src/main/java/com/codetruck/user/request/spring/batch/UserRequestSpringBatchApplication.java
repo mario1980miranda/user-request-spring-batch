cfe36867c45c09e4317c0089c69fb76c160ea871
mario1980miranda <mario_chan15@hotmail.com>
2023-11-19 20:00:28 -0500

    Project created and job configuration

---
+++ src/main/java/com/codetruck/user/request/spring/batch/UserRequestSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.codetruck.user.request.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRequestSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRequestSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetruck/user/request/spring/batch/UserRequestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetruck.user.request.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
