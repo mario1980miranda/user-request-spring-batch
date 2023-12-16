f5422a95fe6141e486f535ab60d1133fb63fe51c
Mario Miranda <mario_chan15@hotmail.com>
2023-12-16 13:55:42 -0500

    Items from code review

--- src/main/java/com/codetruck/user/request/spring/batch/processor/SelectFieldsUserDataProcessorConfig.java
+++ src/main/java/com/codetruck/user/request/spring/batch/processor/SelectFieldsUserDataProcessorConfig.java
@@ -2,7 +2,6 @@ package com.codetruck.user.request.spring.batch.processor;
 
 import com.codetruck.user.request.spring.batch.dto.UserDTO;
 import com.codetruck.user.request.spring.batch.entities.UserRequest;
-import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
